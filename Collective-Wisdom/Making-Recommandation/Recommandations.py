9fcaab72e4f9cd3e9f20a029b51def48583a6f6c
insthink <fuheiwu@126.com>
2016-11-09 18:15:13 +0800

    add use-based cf and transformod func

--- Collective-Wisdom/Making-Recommandation/Recommandations.py
+++ Collective-Wisdom/Making-Recommandation/Recommandations.py
@@ -4,7 +4,17 @@ from math import sqrt
 class Recommandations(object):
     def __init__(self, origin_data):
         self.od = origin_data
-    
+
+    # 将原数据集变为基于物品的
+    def transformod(self):
+        items = list(set([item for p in self.od for item in self.od[p]]))
+
+        trans = {}
+        for item in items:
+            persons = {p: self.od[p][it] for p in self.od for it in self.od[p]}
+            trans[item] = persons
+        self.od = trans
+
     # 计算两人的欧式距离来判断相似度
     def calSimilarity(self, person1, person2):
         # 判断比较的两人是否在数据集里面
@@ -20,7 +30,7 @@ class Recommandations(object):
             for item in common]
 
         return 1 / (1 + sqrt(sum(item_distance)))
-        
+
     # 计算两人的皮尔逊相关系数
     def calPearson(self, person1, person2):
         # 判断比较的两人是否在数据集里面
@@ -28,23 +38,58 @@ class Recommandations(object):
             print "person not in the list"
         # 两人的共同物品
         common = [item for item in self.od[person1] if item in self.od[person2]]
+        n  = len(common)
         if not common:
             return False
-        # 计算总和-平方和-乘积和
+        # 计算X,Y各自期望,等概率分布
         sum1 = sum([self.od[person1][item] for item in common])
         sum2 = sum([self.od[person2][item] for item in common])
+        Ex, Ey = sum1 / n, sum2 / n
 
+        # 计算X^2,Y^2各自期望
         sum1sq = sum([pow(self.od[person1][item], 2) for item in common])
         sum2sq = sum([pow(self.od[person2][item], 2) for item in common])
+        Ex2, Ey2 = sum1sq / n, sum2sq / n
 
+        # 计算X*Y的期望
         cross_sum = sum([self.od[person1][item] * self.od[person2][item] for item in common])
+        Exy = cross_sum / n
+
         # 计算相关系数
-        
+        top = Exy - Ex * Ey
+        bottom = sqrt((Ex2 - pow(Ex, 2)) * (Ey2 - pow(Ey, 2)))
+        if bottom == 0: return 0
+        return top / bottom
+
+    # 在已知数据中寻找与自己品位最相近的topN
+    def topN(self, person, n, similarity = calPearson):
+        scores = [(p, similarity(person, p)) for p in self.od if p != person]
+        scores.sort(key=lambda s:s[1], reverse=True)
+
+        return scores[:n]
+
+
+    def recommandIt(self, person, similarity = calPearson):
+        totals = {}
+        sim_sum = {}
+        for p in self.od:
+            if p == person: continue
+            sim = similarity(person, p)
+
+            if sim <= 0: continue
+            for item in self.od[p]:
+                if item not in self.od[person] or self.od[person][item] == 0:
+                    totals.setdefault(item, 0)
+                    totals[item] += self.od[p][item] * sim
+                    sim_sum.setdefault(item, 0)
+                    sim_sum[item] += sim
+
+        rankings = [(item, total/sim_sum[item]) for item, total in totals.iteritems()]
+        rankings.sort(key=lambda s:s[1], reverse=True)
+        return rankings
 
 
 
-    
-        
 
 if __name__ == "__main__":
     critics = {
@@ -96,12 +141,12 @@ if __name__ == "__main__":
             'Superman Returns': 4.0,
             'You, Me and Dupree': 1.0,
         },
-        'Insthink': {
-            'Snakes on a Plane': 4.5,
-            'Superman Returns': 4.0,
-            'You, Me and Dupree': 1.0,
-        }
     }
     r = Recommandations(critics)
-    simi = r.calSimilarity('Toby', 'Insthink')
-    print simi
+    top_n = r.topN('Toby', 4, similarity=r.calPearson)
+    rank = r.recommandIt('Toby', similarity=r.calPearson)
+    print "------------------"
+    print r.od
+    r.transformod()
+    print r.od
+
